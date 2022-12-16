38f82c364fb62295eac750730e9a17d76bcb286e
DKLMP <anton.belov71@mail.ru>
2022-12-16 09:54:59 +0300

    dwakl;dj;ad

--- personal-task/View/MainWindowPage/MainWindowPage.xaml.cs
+++ personal-task/View/MainWindowPage/MainWindowPage.xaml.cs
@@ -59,11 +59,75 @@ namespace personal_task.View.MainWindowPage
                     File.Create(LastNameUser.SourceLink).Close();
                 }
 
-                GridMenu.Children.Clear();             
+                /*GridMenu.Children.Clear(); */
 
                 LastNameUser.CircleName = (LBMenu.SelectedItem as UserCircle).Circle.CircleName.Trim();
-                FrameNavigate.objectFrame.Navigate(new MainWindowUC());
+                StreamReader streamReader = new StreamReader(LastNameUser.SourceLink);
+                string test = streamReader.ReadToEnd();
+                streamReader.Close();
+                tbcircle.Text = test;
+                /*FrameNavigate.objectFrame.Navigate(new MainWindowUC());*/
             }
+        }
+
+        private void tbcircle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string rtbox = tbcircle.Text;
+            switch (LastNameUser.CircleName)
+            {
+                case "C#":
+                    if (File.Exists(LastNameUser.SourceLink))
+                    {
+
+                        LastNameUser.templink = System.IO.Path.GetDirectoryName(LastNameUser.SourceLink);
+                        Directory.CreateDirectory(LastNameUser.templink);
+                        File.Create(LastNameUser.SourceLink).Close();
+
+                        StreamWriter SWCSharp = new StreamWriter(LastNameUser.SourceLink, true);
+                        SWCSharp.WriteLine(String.Format(rtbox));
+                        SWCSharp.Close();
+                    }
+                    break;
+                case "WPF":
+                    if (File.Exists(LastNameUser.SourceLink))
+                    {
+
+                        LastNameUser.templink = System.IO.Path.GetDirectoryName(LastNameUser.SourceLink);
+                        Directory.CreateDirectory(LastNameUser.templink);
+                        File.Create(LastNameUser.SourceLink).Close();
+
+                        StreamWriter SWCpp = new StreamWriter(LastNameUser.SourceLink, true);
+                        SWCpp.WriteLine(String.Format(rtbox));
+                        SWCpp.Close();
+                    }
+                    break;
+                case "C++":
+                    if (File.Exists(LastNameUser.SourceLink))
+                    {
+
+                        LastNameUser.templink = System.IO.Path.GetDirectoryName(LastNameUser.SourceLink);
+                        Directory.CreateDirectory(LastNameUser.templink);
+                        File.Create(LastNameUser.SourceLink).Close();
+
+                        StreamWriter SWcpp = new StreamWriter(LastNameUser.SourceLink, true);
+                        SWcpp.WriteLine(String.Format(rtbox));
+                        SWcpp.Close();
+
+                    }
+                    break;
+
+
+            }
+        }
+
+        private void BtnFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPublish_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
